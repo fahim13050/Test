3b9d35808953add893a56bc8d5aba1d0bfed53f9
fahim13050 <100145302+fahim13050@users.noreply.github.com>
2022-02-21 21:06:59 +0600

    Up

---
+++ For loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x ;
+
+            for (x=0 ;  x<100 ; x++)
+
+               {         
+             Console.WriteLine("Fahim");
+
+               }
+            Console.ReadKey();
+            //fahim foysla
+        }
+    }
+}
